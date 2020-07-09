72af029269606153a5df703531ac7adaf120c67f
d00d1e <lee.julianay@gmail.com>
2020-07-09 15:51:38 -0700

    End-to-End testing for Filter Events by City feature

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,5 +1,40 @@
 import puppeteer from 'puppeteer';
 
+describe('Filter events by city', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch(
+      // { headless: false, slowMo: 50 }
+    );
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  });
+
+  afterAll(async () => {
+    browser.close();
+  });
+
+  test('Show upcoming events based on the users location by default', async () => {
+    const events = await page.$('.event');
+    expect(events).toBeDefined();
+  });
+
+  test('User should see a list of suggestions when they search for a city', async () => {
+    const suggestions = await page.$('.suggestions li');
+    await page.type('.city', 'Boston')
+    expect(suggestions).toBeDefined();
+  });
+
+  test('User can select a city from the suggested list', async () => {
+    const suggestion = await page.$('.suggestions');
+    expect(suggestion).toBeDefined();
+    expect('.city').toBeDefined();
+  });
+});
+
+
 describe('show/hide an events details', () => {
   let browser;
   let page;
