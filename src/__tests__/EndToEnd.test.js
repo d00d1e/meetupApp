fe183bd0c4662a508416443e6c6dfc1f14d464d5
d00d1e <lee.julianay@gmail.com>
2020-07-12 11:06:01 -0700

    Integration with Atatus

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -52,19 +52,19 @@ describe('show/hide an events details', () => {
   });
 
   test('An event element is collapsed by default', async () => {
-    const extra = await page.$('.event .extra');
-    expect(extra).toBeNull();
+    const eventDetails = await page.$('.event .eventDetails');
+    expect(eventDetails).toBeNull();
   });
 
   test('User can expand an event to see its details', async () => {
     await page.click('.event .detailsButton');
-    const extra = await page.$('event .extra');
-    expect(extra).toBeDefined();
+    const eventDetails = await page.$('event .eventDetails');
+    expect(eventDetails).toBeDefined();
   });
 
   test('User can collapse an event to hide its details', async() => {
     await page.click('.event .detailsButton');
-    const extra = await page.$('.event .extra');
-    expect(extra).toBeNull();
+    const eventDetails = await page.$('.event .eventDetails');
+    expect(eventDetails).toBeNull();
   });
 });
