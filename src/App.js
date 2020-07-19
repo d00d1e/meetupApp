f07f494e53106efd934300b26b2ef191a3943b05
d00d1e <lee.julianay@gmail.com>
2020-07-19 16:19:01 -0700

    Data visualization with Rechart, scatter chart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
+import moment from 'moment';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import './App.css'; 
+
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
@@ -11,7 +14,7 @@ class App extends Component {
   //load a list of events by default
   componentDidMount() {
     getEvents().then(response => this.setState({ events: response }));
-    window.addEventListenter('online', this.offLineAlert())
+    window.addEventListener('online', this.offlineAlert());
   }
   
   state = {
@@ -24,10 +27,10 @@ class App extends Component {
 
   offlineAlert = () => {
     if(navigator.onLine === false) {
-      this.setState({ alertText: 'You are offline, please connect to internet for an updated list' });
+      this.setState({ alertText: 'You are currently offline, please connect to internet for an updated list' });
     } else {
       this.setState({ alertText: '' });
-    }
+    } 
   }
 
   updateEvents = (lat, lon, page) => {
@@ -38,7 +41,31 @@ class App extends Component {
     } else {
       getEvents(this.state.lat, this.state.lon, this.state.page).then(events => this.setState({ events }));
     }
-  };
+  }
+
+  countEventsOnADate = (date) => {
+    let count = 0;
+    for (let i = 0; i < this.state.events.length; i += 1) {
+      if (this.state.events[i].local_date === date) {
+        count += 1;
+      }
+    }
+    return count;
+  }
+
+  //get correctly formatted date for upcomimng 7 days
+  getData = () => {
+    const next7Days = [];
+    const currentDate = moment();
+    for (let i = 0; i < 7; i += 1) {
+      currentDate.add(1, 'days');
+      const dateString = currentDate.format('YYYY-MM-DD');
+      const count = this.countEventsOnADate(dateString);
+      next7Days.push({ date: dateString, number: count });
+    }
+    return next7Days;
+  }
+
 
   render() {
     return (
@@ -46,6 +73,18 @@ class App extends Component {
         <CitySearch updateEvents={this.updateEvents} />
         <NumberOfEvents updateEvents={this.updateEvents} />
         <OfflineAlert text={this.state.alertText} />
+
+        <ResponsiveContainer height={240} >
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }} >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="date" name="date" />
+            <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+        
+
         <EventList events={this.state.events} />
       </div>
     );
